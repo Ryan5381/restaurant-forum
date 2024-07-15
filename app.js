a2c915674cdf585822c1c7a408b5b1dc07410a9f
Wuyoucen <youcen666@gamil.com>
2024-07-16 03:41:19 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express')
+const handlebars = require('express-handlebars')
 const routes = require('./routes')
 
 const app = express()
 const port = process.env.PORT || 3000
 
+app.engine('hbs', handlebars({extends: '.hbs'}))
+app.set('view engine', 'hbs')
+
 app.use(routes)
 
 app.listen(port, () => {
+  ;
   console.info(`Example app listening on port ${port}!`)
 })
 
