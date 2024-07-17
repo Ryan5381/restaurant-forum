e0cbae49f9cd1eaa760bb36360b4430ab5fec8af
Wuyoucen <youcen666@gamil.com>
2024-07-17 20:44:24 +0800

    feat: user model add is_admin

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ const admin = require('./modules/admin')
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
 
+const { authenticated } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler') // 新增這行
 
 
@@ -20,7 +21,7 @@ router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn) // 注意是 post
 router.get('/logout', userController.logout)
 
-router.get('/restaurants', restController.getRestaurants)
+router.get('/restaurants', authenticated, restController.getRestaurants)
 
 router.use('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler) 
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -4,8 +4,9 @@ const router = express.Router()
 
 
 const adminController = require('../../controllers/admin-controller')
+const { authenticatedAdmin } = require('../../middleware/auth') 
 
-router.get('/restaurants', adminController.getRestaurants)
+router.get('/restaurants', authenticatedAdmin, adminController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
 
 module.exports = router
