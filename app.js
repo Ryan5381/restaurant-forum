8d1c6dffd2a886cd7bacd324da0a739a60483bac
Wuyoucen <youcen666@gamil.com>
2024-07-17 18:57:04 +0800

    feat: add header & footer

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const handlebars = require('express-handlebars')
 const flash = require('connect-flash')
 const session = require('express-session')
 const passport = require('./config/passport') // 引入 Passport
+const handlebarsHelpers = require('./helpers/handlebars-helpers')
+const { getUser } = require('./helpers/auth-helpers')
 
 const routes = require('./routes')
 
@@ -12,7 +14,7 @@ const app = express()
 const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret'
 
-app.engine('hbs', handlebars({extname: '.hbs'}))
+app.engine('hbs', handlebars({extname: '.hbs', helpers: handlebarsHelpers}))
 app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
@@ -24,6 +26,7 @@ app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
   res.locals.warning_msg = req.flash('warning_msg')
+  res.locals.user = getUser(req) 
   next()
 })
 
