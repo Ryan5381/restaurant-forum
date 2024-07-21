adfc32cfcbb2c3156fc42d3ebd30619df62d1da2
Wuyoucen <youcen666@gamil.com>
2024-07-22 03:26:17 +0800

    user_is_admin

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,4 +1,11 @@
 const dayjs = require('dayjs')
+const Handlebars = require('handlebars')
+
+Handlebars.registerHelper('eq', function (a, b) {
+  return a === b
+})
+
 module.exports = {
-  currentYear: () =>dayjs().year()
+  currentYear: () =>dayjs().year(),
+  eq: (a, b) => a === b
 }
--- seeders/20240721175029-users-seed-file.js
+++ seeders/20240721175029-users-seed-file.js
@@ -1,9 +1,11 @@
 'use strict';
 const bcrypt = require('bcryptjs')
+const faker = require('faker');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
+    // 測試資料
     await queryInterface.bulkInsert('Users', [{
       email: 'root@example.com',
       password: await bcrypt.hash('12345678', 10),
@@ -18,18 +20,30 @@ module.exports = {
       name: 'user1',
       created_at: new Date(),
       updated_at: new Date()
-      }, {
+    }, {
       email: 'user2@example.com',
       password: await bcrypt.hash('12345678', 10),
       is_admin: false,
       name: 'user2',
       created_at: new Date(),
       updated_at: new Date()
-      }
-    ], {});
+    }
+    ], {})
+
+    // Faker生成的用户数据
+    const fakeUsers = Array.from({ length: 50 }, () => ({
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+      is_admin: false,
+      created_at: new Date(),
+      updated_at: new Date()
+    }));
+
+    await queryInterface.bulkInsert('Users', fakeUsers, {});
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     await queryInterface.bulkDelete('Users', {});
   }
 };
