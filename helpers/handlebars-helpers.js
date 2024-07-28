1e213e27a2c27166343a370b141160eae54b78d2
Wuyoucen <youcen666@gamil.com>
2024-07-28 19:14:55 +0800

    feat: add categories navbar on restaurants index page

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -6,7 +6,7 @@ Handlebars.registerHelper('eq', function (a, b) {
 })
 
 module.exports = {
-  currentYear: () =>dayjs().year(),
+  currentYear: () => dayjs().year(),
   eq: (a, b) => a === b,
   ifCond: function (a, b, options) {
     return a === b ? options.fn(this) : options.inverse(this)
