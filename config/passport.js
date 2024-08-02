112d5cb3b63acb3beca9ba0ee46af508ad7b01e8
Wuyoucen <youcen666@gamil.com>
2024-08-02 19:17:23 +0800

    feat: create followship model

--- config/passport.js
+++ config/passport.js
@@ -32,14 +32,15 @@ passport.deserializeUser((id, done) => {
   User.findByPk(id, {
     include:[
       { model: Restaurant, as: 'FavoritedRestaurants'},
-      { model: Restaurant, as: 'LikedRestaurants'}
+      { model: Restaurant, as: 'LikedRestaurants'},
+      { model: User, as: 'Followers' },
+      { model: User, as: 'Followings' }
     ]
   })
   .then(user => {
-    console.log(user)
     done(null, user.toJSON())
   })
   .catch(err => done(err))
-  })
+})
 
 module.exports = passport
